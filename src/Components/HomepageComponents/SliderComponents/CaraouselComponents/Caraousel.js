c4f21b599c28c9bd99f52bfbe8dfe1024de5563f
Praneet <praneetdebnath@yahoo.com>
2024-06-17 16:44:28 +0530

    caraousel changes

--- src/Components/HomepageComponents/SliderComponents/CaraouselComponents/Caraousel.js
+++ src/Components/HomepageComponents/SliderComponents/CaraouselComponents/Caraousel.js
@@ -2,9 +2,41 @@ import React, { useState } from "react";
 import BoxOne from "./BoxOne";
 import BoxTwo from "./BoxTwo";
 import './Box.css';
+import Radio from '@mui/material/Radio';
 
 const Carousel = () => {
     const [slide, setSlide] = useState(false);
+
+    const [selectedValue1, setSelectedValue1] = React.useState('c');
+    const [selectedValue2, setSelectedValue2] = React.useState('a');
+
+    const handleChange1 = (event) => {
+        setSelectedValue1(event.target.value);
+        setSelectedValue2('a');
+        handleSlide();
+    };
+
+    const handleChange2 = (event) => {
+        setSelectedValue2(event.target.value);
+        setSelectedValue1('a');
+        handleSlide();
+    };
+
+    const controlProps1 = (item) => ({
+        checked: selectedValue1 === item,
+        onChange: handleChange1,
+        value: item,
+        name: 'size-radio-button-demo',
+        inputProps: { 'aria-label': item },
+    });
+
+    const controlProps2 = (item) => ({
+        checked: selectedValue2 === item,
+        onChange: handleChange2,
+        value: item,
+        name: 'size-radio-button-demo',
+        inputProps: { 'aria-label': item },
+    });
 
     const handleSlide = () => {
         setSlide(!slide);
@@ -19,9 +51,22 @@ const Carousel = () => {
                 </div>
             </div>
             <div className="buttonOuter">
-                <div className="buttonInner">
-                    <button className="button" onClick={handleSlide}>Slide</button>
-                </div>
+                    <Radio
+                        {...controlProps1('c')}
+                        sx={{
+                            '& .MuiSvgIcon-root': {
+                                fontSize: 28,
+                            },
+                        }}
+                    />
+                    <Radio
+                        {...controlProps2('c')}
+                        sx={{
+                            '& .MuiSvgIcon-root': {
+                                fontSize: 28,
+                            },
+                        }}
+                    />
             </div>
         </div>
     );
